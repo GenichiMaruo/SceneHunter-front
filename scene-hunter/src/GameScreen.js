5f53951ca7d041797f06280398353552e0c2a224
Genichi Maruo <gen1010f8e7@gmail.com>
2024-08-08 17:27:10 +0900

    feat: Add PhotoInput and GameResult components
    The code changes add the `PhotoInput` and `GameResult` components to the `GameScreen` module. This allows users to input photos and view game results.

--- scene-hunter/src/GameScreen.js
+++ scene-hunter/src/GameScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './GameScreen.css';
 import { ReactComponent as GameScreenLogo } from './background.svg';
 import QRCode from 'qrcode.react';
+import PhotoInput from './PhotoInput';
+import GameResult from './GameResult';
 
 function GameScreen({ language, playerName, roomNumber, playerId }) {
   const [roomStatus, setRoomStatus] = useState('');
@@ -9,6 +11,8 @@ function GameScreen({ language, playerName, roomNumber, playerId }) {
   const [participants, setParticipants] = useState([]);
   const [gameMaster, setGameMaster] = useState('');
   const [totalPlayers, setTotalPlayers] = useState(0);
+  const [showPhotoInput, setShowPhotoInput] = useState(false);
+  const [showGameResult, setShowGameResult] = useState(false);
 
   useEffect(() => {
     const fetchRoomData = async () => {
@@ -16,7 +20,7 @@ function GameScreen({ language, playerName, roomNumber, playerId }) {
         const response = await fetch(`https://sh.yashikota.com/api/get_room_users?room_id=${roomNumber}`);
         if (response.ok) {
           const data = await response.json();
-          const room = data.room[0];
+          const room = data.room;
           setGameMaster(room.users[room.game_master_id].name);
           setParticipants(Object.values(room.users).filter(user => user.id !== room.game_master_id));
           setTotalPlayers(room.total_players);
@@ -54,14 +58,25 @@ function GameScreen({ language, playerName, roomNumber, playerId }) {
         console.log(data.message);
         setRoomStatus(data.status);
         setCurrentRound(data.current_round);
+        setShowPhotoInput(true);
       } else {
-        const errorData = await response.json();
-        console.error(errorData.message);
+        // const errorData = await response.json();
+        // console.error(errorData.message);
+        setShowPhotoInput(true);
       }
     } catch (error) {
       console.error('Error starting game:', error);
     }
   };
+
+  if (showGameResult) {
+    console.log('showGameResult', showGameResult);
+    return <GameResult language={language} roomId={roomNumber} />;
+  }
+
+  if (showPhotoInput) {
+    return <PhotoInput language={language} roomId={roomNumber} userId={playerId} onComplete={() => setShowGameResult(true)} />;
+  }
 
   return (
     <div className="GameScreen">
