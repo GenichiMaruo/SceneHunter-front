7d8cf3e9a9b4c24bbac7f141c8ecfeaa8fbbaabd
Genichi Maruo <gen1010f8e7@gmail.com>
2024-08-11 03:32:59 +0900

    refactor: Update GameScreen.css and GameScreen.jsx

--- scene-hunter/src/GameScreen.jsx
+++ scene-hunter/src/GameScreen.jsx
@@ -11,6 +11,7 @@ function GameScreen({ language, playerName, roomNumber, playerId }) {
   const [currentRound, setCurrentRound] = useState(1);
   const [participants, setParticipants] = useState([]);
   const [gameMaster, setGameMaster] = useState('');
+  const [gameMasterId, setGameMasterId] = useState('');
   const [totalPlayers, setTotalPlayers] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [showPhotoInput, setShowPhotoInput] = useState(false);
@@ -24,6 +25,7 @@ function GameScreen({ language, playerName, roomNumber, playerId }) {
           const data = await response.json();
           const room = data.room;
           setGameMaster(room.users[room.game_master_id].name);
+          setGameMasterId(room.game_master_id);
           setParticipants(Object.values(room.users).filter(user => user.id !== room.game_master_id));
           setTotalPlayers(room.total_players);
           setRoomStatus(room.status);
@@ -46,12 +48,17 @@ function GameScreen({ language, playerName, roomNumber, playerId }) {
         console.log('Received event:', data);
 
         switch (data.message) {
-          case 'change game master':
-          case 'update user name':
-          case 'update number of users':
           case 'update game rounds':
           case 'update game status':
           case 'update photo uploaded users':
+            // ゲーム開始された場合
+            setRoomStatus(data.status);
+            setCurrentRound(data.current_round);
+            setShowPhotoInput(true);
+            break;
+          case 'change game master':
+          case 'update user name':
+          case 'update number of users':
             fetchRoomData();
             break;
           default:
@@ -72,7 +79,7 @@ function GameScreen({ language, playerName, roomNumber, playerId }) {
 
   const handleStartGame = async () => {
     try {
-      const response = await fetch(`https://sh.yashikota.com/api/start_game?room_id=${roomNumber}`, {
+      const response = await fetch(`https://sh.yashikota.com/api/game_start?room_id=${roomNumber}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -105,7 +112,6 @@ function GameScreen({ language, playerName, roomNumber, playerId }) {
   };
 
   if (showGameResult) {
-    console.log('showGameResult', showGameResult);
     return <GameResult language={language} roomId={roomNumber} />;
   }
 
@@ -136,13 +142,21 @@ function GameScreen({ language, playerName, roomNumber, playerId }) {
               <span role="img" aria-label="crown" className="crown" >👑</span>
             </li>
             {participants.map((player) => (
-              <li key={player.id}>{player.name}</li>
+              <li key={player.id} className={player.id === playerId ? 'GameScreen-underline' : ''}>
+                {player.name}
+              </li>
             ))}
           </ul>
         </div>
-        <button className="GameScreen-startButton" onClick={handleStartGame}>
-          {language === 'jp' ? 'このメンバーでゲームを始める' : 'Start the game with these members'}
-        </button>
+        {playerId === gameMasterId ? (
+          <button className="GameScreen-startButton" onClick={handleStartGame}>
+            {language === 'jp' ? 'このメンバーでゲームを始める' : 'Start the game with these members'}
+          </button>
+        ) : (
+          <p className="GameScreen-waitingMessage">
+            {language === 'jp' ? 'ゲーム開始を待機中' : 'Waiting for the game to start'}
+          </p>
+        )}
       </main>
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <QRCodeSVG value={`https://scene-hunter.pages.dev/${roomNumber}`} size={256} />
