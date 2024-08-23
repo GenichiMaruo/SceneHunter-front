cd0b08eef5234f44d831c7500a8a9df592553ac3
Genichi Maruo <gen1010f8e7@gmail.com>
2024-08-24 01:35:45 +0900

    tokenに対応

--- scene-hunter/src/WaitingScreen.jsx
+++ scene-hunter/src/WaitingScreen.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import './WaitingScreen.css';
 
 function WaitingScreen({ language, isGameMaster }) {
-  return (
-    <div className="WaitingScreen">
-      <div className="WaitingScreen-content">
-        <div className="WaitingScreen-spinner"></div>
-        <p className="WaitingScreen-text">
-          {isGameMaster
-            ? (language === 'jp' ? '他のプレイヤーが撮影中です。お待ちください...' : 'The game master is taking a photo. Please wait...')
-            : (language === 'jp' ? 'ゲームマスターが撮影中です。お待ちください...' : 'Another player is taking a photo. Please wait...')}
-        </p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="WaitingScreen">
+            <div className="WaitingScreen-content">
+                <div className="WaitingScreen-spinner"></div>
+                <p className="WaitingScreen-text">
+                    {isGameMaster
+                        ? (language === 'ja' ? '他のプレイヤーが撮影中です。お待ちください...' : 'The game master is taking a photo. Please wait...')
+                        : (language === 'ja' ? 'ゲームマスターが撮影中です。お待ちください...' : 'Another player is taking a photo. Please wait...')}
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default WaitingScreen;
