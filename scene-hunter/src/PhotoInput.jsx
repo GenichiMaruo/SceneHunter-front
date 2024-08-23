f9564a39011dacd99ec312077c912b50b1b833cb
Genichi Maruo <gen1010f8e7@gmail.com>
2024-08-24 02:28:51 +0900

    jaeg->jpg（２回目）

--- scene-hunter/src/PhotoInput.jsx
+++ scene-hunter/src/PhotoInput.jsx
@@ -85,7 +85,7 @@ function PhotoInput({ token, apiUrl, language, roomId, userId, isGameMaster, set
     const context = canvasRef.current.getContext('2d');
 
     context.drawImage(videoRef.current, 0, 0, canvasWidth, canvasHeight);
-    const dataUrl = canvasRef.current.toDataURL('image/jaeg');
+    const dataUrl = canvasRef.current.toDataURL('image/jpg');
     await uploadPhoto(dataUrl);
 
     finishCapture();
